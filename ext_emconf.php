61106d69daa9e8679a014266f97bdc197a5b7f75
Leon de Rijke <leon@cmsworks.nl>
2014-04-16 15:08:59 +0200

    Initial working version

---
+++ ext_emconf.php
@@ -0,0 +1,54 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "cwfluidyoutube".
+ *
+ * Auto generated 16-04-2014 13:51
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Provider extension for content ',
+	'description' => 'Provider extension for content ',
+	'category' => 'misc',
+	'shy' => 0,
+	'version' => '0.0.1',
+	'dependencies' => 'cms,extbase,fluid,flux,fluidcontent,vhs',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'experimental',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 1,
+	'lockType' => '',
+	'author' => 'Leon de Rijke',
+	'author_email' => 'leon@cmsworks.nl',
+	'author_company' => 'CMS Works B.V.',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '6.2-6.2.99',
+			'cms' => '',
+			'extbase' => '',
+			'fluid' => '',
+			'flux' => '',
+			
+			'fluidcontent' => '',
+			'vhs' => '',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:0:{}',
+	'suggests' => array(
+	),
+);
